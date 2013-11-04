09a57d327bb44550d7964d975e6d55f17d6d31cb
Taras Danylak <tdanylak@yorku.ca>
2013-11-04 11:13:15 -0500

    Creating a default config file

---
+++ config/papyrus_app_config.default.rb
@@ -0,0 +1,19 @@
+## SAMPLE CONFIGURATION OPTIONS
+
+PapyrusConfig.configure do |config|
+
+  ## ORGANIZATION
+    
+  config.organization.full_name
+  config.organization.short_name
+  config.organication.app_url
+  
+  ## AUTHENTICATION
+  
+  ## ERRORS
+  
+  ## NOTIFICATIONS
+  
+  ## BIB SEARCH
+
+end
