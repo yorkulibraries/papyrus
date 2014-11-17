ae5eaa70df818e7b17d88c6bd4cab3beca4e5518
Taras Danylak <tdanylak@yorku.ca>
2014-11-17 12:01:32 -0500

    Bump version to 2.3.2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,41 +1,41 @@
 module ApplicationHelper
-  
+
   def controller_name
     controller.controller_name
   end
-  
+
   def is_action?(a, output = nil)
     result = controller.action_name == a
      output != nil && result ? output : result
   end
-  
+
   def is_controller?(c, output = nil)
     result = controller.controller_name == c
    output != nil && result ? output : result
   end
-  
+
   def is_controller_and_action?(c, a, output = nil)
     result = (controller.controller_name == c && controller.action_name == a)
     output != nil && result ? output : result
   end
-    
-  
+
+
   def action_name
     controller.action_name
   end
 
   def current_path
-    path = request.env['PATH_INFO']    
+    path = request.env['PATH_INFO']
   end
-  
+
   def papyrus_version
-    "2.3.1"
+    "2.3.2"
   end
-  
+
   def papyrus_institution
     PapyrusConfig.organization.full_name
   end
-  
+
   def org_short_name
     PapyrusConfig.organization.short_name
   end
