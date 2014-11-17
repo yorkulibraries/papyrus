4f8f683c47dcaaf044ed442bfa0c2f64bc5595f5
Taras Danylak <tdanylak@yorku.ca>
2014-11-17 11:38:11 -0500

    QuickFix to add Preferred Format to Students stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,50 +4,51 @@ class StatsController < ApplicationController
   def index
     @has_attachments_count = Attachment.count("item_id", distinct: true)
     @has_students_count = ItemConnection.count("item_id", distinct: true)
-    
+
   end
 
   def generate
-    if params[:assigned_to]
+    if params[:assigned_to] != "all"
       id = params[:assigned_to]
       assigned_to = User.find(id)
       @title = "Students Assgined to #{assigned_to.name}"
       @results = Student.includes(:student_details).joins(:student_details).where("transcription_coordinator_id = ? or transcription_assistant_id = ?", id, id)
     else
-      @results = Array.new
+      @title = "All Students"
+      @results =  Student.active.includes(:student_details).joins(:student_details)
     end
-    
+
     respond_to do |format|
       format.html
-      format.csv { send_data to_csv(@results)}
-      format.xls 
+      format.csv { send_data to_csv(@results) }
+      format.xls
     end
   end
-  
-  
+
+
   def item_usage
-    
+
     @start_date = params["start_date"] || Date.parse("2009-01-31")
     @end_date = params["end_date"] || Date.today
     @end_date = Date.parse(@end_date) if @end_date.is_a? String
-    
+
     select_item_fields = "items.title, items.id, items.callnumber, items.isbn, items.source"
     select_item_connections_fields = "count(item_connections.student_id) as assigned_count, item_connections.created_at as assigned_at"
     select_fields = select_item_connections_fields + ", " + select_item_fields
     where_clause = "where item_connections.created_at >= '#{@start_date}' AND item_connections.created_at < '#{@end_date + 1.day}'"
-    
+
     sql = "SELECT #{select_fields} FROM item_connections INNER JOIN items ON item_connections.item_id = items.id #{where_clause} GROUP BY item_connections.item_id"
-    
-    @export_items = @assigned_items = ActiveRecord::Base.connection.exec_query(sql)    
-                
-    
+
+    @export_items = @assigned_items = ActiveRecord::Base.connection.exec_query(sql)
+
+
     @items_count = Item.count
-    
+
     @unassigned_items = Item.where("id not in (?)", @assigned_items.collect{ |i| i["id"] })
-    
+
     respond_to do |format|
       format.html
-      format.xls 
+      format.xls
     end
   end
 
@@ -55,23 +56,23 @@ class StatsController < ApplicationController
   def authorize_check
      authorize! :show, :stats
   end
-  
+
   def to_csv(data)
     require 'csv'
-    
+
     CSV.generate(Hash.new) do |csv|
-      added_headers = false  
+      added_headers = false
 
       data.each do |result|
         if result.is_a?(Student)
           unless added_headers
-            csv << %w(id name email student_number cds_adviser created_on)
+            csv << %w(id name email student_number preferred_formats cds_adviser created_on)
             added_headers = true
           end
           csv << result.to_csv
         end
       end
     end
-    
+
   end
 end
--- app/models/student_details.rb
+++ app/models/student_details.rb
@@ -1,46 +1,58 @@
 class StudentDetails < ActiveRecord::Base
-  attr_accessible  :student_number, :home_phone, :campus_phone, :cell_phone, :work_phone, 
-                    :di_ld, :di_low_vision, :di_blind, :di_braille_labels_required, :di_head_injury, :di_partially_sighted, :di_other, :di_note, 
+  attr_accessible  :student_number, :home_phone, :campus_phone, :cell_phone, :work_phone,
+                    :di_ld, :di_low_vision, :di_blind, :di_braille_labels_required, :di_head_injury, :di_partially_sighted, :di_other, :di_note,
                     :sp_psmds, :sp_lds, :sp_mhds, :sp_glendon_counselling,
-                    :format_large_print, :format_pdf, :format_kurzweil, :format_daisy, :format_braille, :format_word, :pf_note, :wrms_ref_number, 
+                    :format_large_print, :format_pdf, :format_kurzweil, :format_daisy, :format_braille, :format_word, :pf_note, :wrms_ref_number,
                     :transcription_coordinator_id, :transcription_assistant_id, :cds_adviser,
-                    :request_form_signed_on, :intake_appointment_on, :responsibilities_document_signed_on, 
+                    :request_form_signed_on, :intake_appointment_on, :responsibilities_document_signed_on,
                     :ppy_access_granted_on, :adaptive_computed_access_granted_on, :requires_orientation, :orientation_completed, :orientation_completed_at
-                    
+
   belongs_to :student
   belongs_to :transcription_coordinator, class_name: "User", foreign_key: "transcription_coordinator_id"
   belongs_to :transcription_assistant, class_name: "User", foreign_key: "transcription_assistant_id"
-  
+
   acts_as_audited associated_with: :student
-  
+
 
   validates_presence_of :student_number, :cell_phone, :cds_adviser
   validates_presence_of  :transcription_coordinator, message: "You must specify a transcription coordinator."
   validates_presence_of :transcription_assistant, message: "You must specify a transcritpion assistant"
-  
-  
+
+
   def coordinator
-    if self.transcription_coordinator.blank?      
-      return User.new 
+    if self.transcription_coordinator.blank?
+      return User.new
     else
       return self.transcription_coordinator
     end
   end
-  
+
   def assistant
-    if self.transcription_assistant.blank?      
-      return User.new 
+    if self.transcription_assistant.blank?
+      return User.new
     else
       return self.transcription_assistant
     end
   end
-  
-  
+
+
+  def formats
+    formats_array = Array.new
+    formats_array.push "PDF" if self.format_pdf
+    formats_array.push "KURZWEIL" if self.format_kurzweil
+    formats_array.push "DAISY" if self.format_daisy
+    formats_array.push "BRAILLE" if self.format_braille
+    formats_array.push "WORD DOCUMENT" if self.format_word
+    formats_array.push "LARGE PRINT" if self.format_large_print
+
+    formats_array
+  end
+
   def complete_orientation
     self.orientation_completed = true
     self.orientation_completed_at = Date.today
-    save(valudate: false)    
+    save(valudate: false)
   end
-  
-  
+
+
 end
