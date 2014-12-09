5bc1a551a9db927461950b068a19f18b2c5b2fde
Taras Danylak <tdanylak@yorku.ca>
2014-12-09 15:19:20 -0500

    Fixing a few minor bugs

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,33 +2,35 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    
+
     user ||= User.new
-        
-  
+
+
     if user.role == User::ADMIN
       can :manage, :all
     elsif user.role == User::MANAGER
       can :show, :dashboard
-      
+
       can :manage, [Item, Student, Attachment, ItemConnection, User, Note, Term, Course]
       can :show, :stats
-      can :manage, :acquisitions      
-      
+      can :manage, :acquisitions
+
       can :login_as, :student
-      
-    elsif  user.role == User::COORDINATOR  ||  user.role == User::STAFF    
+
+    elsif  user.role == User::COORDINATOR  ||  user.role == User::STAFF
       can :show, :dashboard
-      
+
       can :manage, [Term, Course]
-      can [:read, :create, :update], [Note, AccessCode]
+      can :manage, AcquisitionRequest
+      can :manage, AccessCode
+      can [:read, :create, :update], [Note]
       can :manage, [Item, Attachment, ItemConnection]
-      can [:read, :create, :update, :items, :notify, :send_welcome_email, :audit_trail, :reactivate, :inactive, :destroy], Student     
+      can [:read, :create, :update, :items, :notify, :send_welcome_email, :audit_trail, :reactivate, :inactive, :destroy], Student
       can :login_as, :student
     elsif user.role == User::PART_TIME
       can :show, :dashboard
-      
-      can [:create, :read, :update], [Item, Note, Course] 
+
+      can [:create, :read, :update], [Item, Note, Course]
       can [:assign_to_students, :assign_many_to_student, :withhold_from_student, :courses], Item
       can [:assign_to_item, :add_item, :remove_item], Course
       can :manage, ItemConnection
@@ -36,26 +38,26 @@ class Ability
       can :login_as, :student
       can :create, Attachment
       can :get_file, Attachment
-      
+
     elsif user.role == User::ACQUISITIONS
       can :show, :dashboard
-      
+
       can :manage, AcquisitionRequest
       can :read, Item
       can :create, Attachment
       can :get_file, Attachment
       can :zipped_files, Item
-    else      
+    else
       can :show, :student
       can :zipped_files, Item
       can :get_file, Attachment
     end
-    
-    
-    # global search 
-    
+
+
+    # global search
+
     can [:search, :search_courses], :all
-    
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
