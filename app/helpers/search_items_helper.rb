f82a9d5d3223498b89fcdce0fd95a31763a39f9a
Taras Danylak <tdanylak@yorku.ca>
2015-01-19 11:06:50 -0500

    Fixing the incompatible encoding issues

--- app/helpers/search_items_helper.rb
+++ app/helpers/search_items_helper.rb
@@ -1,15 +1,16 @@
 module SearchItemsHelper
-  
+
   def bib_search_solr_label
     PapyrusConfig.bib_search.solr.label
   end
   def bib_search_worldcat_label
     PapyrusConfig.bib_search.worldcat.label
   end
-  
-  
+
+
   def format_solr_field(field, field_name = nil)
-    if field.blank? 
+
+    if field.blank?
        if field_name == nil
          content_tag(:span, "Not provided...", class: "weak")
        else
@@ -19,11 +20,18 @@ module SearchItemsHelper
       if field.kind_of?(Array)
         field.reject { |i| i.blank? }.join(", ")
       else
-        field
+        field = encode_field(field) rescue "#{Encoding.default_external}"
       end
     end
-    
+
   end
   alias :format_worldcat_field :format_solr_field
-  
+
+  def encode_field(field)
+    if field.encoding.name != "UTF-8"
+      field.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+    end
+
+  end
+
 end
