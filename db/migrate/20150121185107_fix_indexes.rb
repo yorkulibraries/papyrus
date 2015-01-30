ad60b421e41a529746afec69a9608b6bafd76f7f
Taras Danylak <tdanylak@yorku.ca>
2015-01-30 11:15:27 -0500

    Fixing an issue with spelling on fix_indexes migration

--- db/migrate/20150121185107_fix_indexes.rb
+++ db/migrate/20150121185107_fix_indexes.rb
@@ -9,7 +9,7 @@ class FixIndexes < ActiveRecord::Migration
 
 
     # add shorter named indexes
-    add_index :item_course_connections, [:course_id, :item_id],  name: "icc_c_i_index" unless index_exists?(:item_course_conections, [:course_id, :item_id], name: "icc_c_i_index")
+    add_index :item_course_connections, [:course_id, :item_id],  name: "icc_c_i_index" unless index_exists?(:item_course_connections, [:course_id, :item_id], name: "icc_c_i_index")
 
     add_index :item_connections, [:expires_on, :item_id],  name: "ic_e_i_index" unless index_exists?(:item_connections, [:expires_on, :item_id], name: "ic_e_i_index")
     add_index :item_connections, [:expires_on, :student_id],  name: "ic_e_s_index" unless index_exists?(:item_connections, [:expires_on, :student_id], name: "ic_e_s_index")
