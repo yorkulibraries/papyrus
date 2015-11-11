43c821ecbaf9af1994e27788e5734eb77564d40c
Taras Danylak <tdanylak@yorku.ca>
2015-11-11 15:34:35 -0500

    Working on student stylings

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,10 @@ module UsersHelper
   def email_hint
     "#{PapyrusSettings.org_short_name} Email"
   end
+
+  def gravatar(email, size: "64", style: "retro", force: false)
+     email_hash =  Digest::MD5.hexdigest(email)
+     force_default = force ? "f=y" : ""
+     image_tag("http://www.gravatar.com/avatar/#{email_hash}?s=#{size}&d=#{style}&#{force_default}")
+  end
 end
