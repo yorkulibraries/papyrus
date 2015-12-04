d811d31a11e21fb962004c188e753c6d125dcdc5
Taras Danylak <tdanylak@yorku.ca>
2015-12-04 12:30:56 -0500

    Fixed test bugs

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -14,24 +14,24 @@ class SearchControllerTest < ActionController::TestCase
     create(:item, author: "john smith")
 
 
-    get :index, q: "unique"
+    get :items, q: "unique"
     search_results = assigns(:search_results)
     assert_equal "local", search_results, "search results should be set to local"
     assert_response :success
-    assert_template :index
+    assert_template :items
 
     items = assigns(:items)
     assert_equal "unique title", items.first.title
 
-    get :index, q: "9-233"
+    get :items, q: "9-233"
     items = assigns(:items)
     assert_equal "9-233-12345", items.first.isbn
 
-    get :index, q: "000111"
+    get :items, q: "000111"
     items = assigns(:items)
     assert_equal "000111", items.first.unique_id
 
-    get :index, q: "john"
+    get :items, q: "john"
     items = assigns(:items)
     assert_equal "john smith", items.first.author
 
