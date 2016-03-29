4b00ee0ef4e88a7cac9c7e08315282ff6d17e3cd
Taras Danylak <tdanylak@yorku.ca>
2016-03-29 14:02:54 -0400

    fixing travis error

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -40,7 +40,7 @@ module LayoutHelper
      end
   end
 
-  def panel(title:, title_class: "panel-heading", icon: 'th', icon_class: '', styles: 'panel-default', &block)
+  def panel(title: "untitled", title_class: "panel-heading", icon: 'th', icon_class: '', styles: 'panel-default', &block)
     icon_tag = content_tag(:i, "", class: "fa fa-#{icon} #{icon_class}")
     title_tag = content_tag(:strong, icon_tag + " #{title}")
 
