efab1500cd4713364e598c2441c133d1d4705f3f
Taras Danylak <tdanylak@yorku.ca>
2016-10-25 12:40:53 -0400

    testing request headers

--- app/controllers/student_view_controller.rb
+++ app/controllers/student_view_controller.rb
@@ -32,6 +32,10 @@ class StudentViewController < ApplicationController
 
   def details
     @student_details = @student.student_details
+
+    @username = request.headers[PapyrusSettings.auth_cas_header]
+    @alt_username = request.headers[PapyrusSettings.auth_cas_header_alt]
+    @courses = request.headers[PapyrusSettings.course_listing_header]
   end
 
 
