a5df56b25e5dab7b76ebd0e1db448db4506c7ba0
Taras Danylak <tdanylak@yorku.ca>
2016-11-14 09:51:52 -0500

    Profiler

--- config/initializers/profiler.rb
+++ config/initializers/profiler.rb
@@ -2,5 +2,11 @@ require 'rack-mini-profiler'
 
 Rack::MiniProfilerRails.initialize!(Rails.application)
 
+
+if Rails.env.production? or Rails.env.staging?
+  Rails.application.middleware.delete(Rack::MiniProfiler)
+  Rails.application.middleware.insert_after(Rack::Deflater, Rack::MiniProfiler)
+end
+
 Rack::MiniProfiler.config.position = 'right'
 Rack::MiniProfiler.config.start_hidden = true
