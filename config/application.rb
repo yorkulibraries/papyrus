7dedc86713a41dcd7f6263c53c9e0922465ccf45
Taras Danylak <tdanylak@yorku.ca>
2017-06-29 09:09:32 -0400

    Moving lib files around

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,7 @@ module Papyrus
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths << "#{Rails.root}/lib"
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -56,6 +57,6 @@ module Papyrus
 
 
     config.i18n.enforce_available_locales = false
-    
+
   end
 end
