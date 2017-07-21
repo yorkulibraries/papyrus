5be8774812da90a75197d93575aeadf6663cce31
Taras Danylak <tdanylak@yorku.ca>
2017-07-21 14:15:08 -0400

    Removed profiler

--- config/initializers/profiler.rb
+++ config/initializers/profiler.rb
@@ -1,6 +1,6 @@
-require 'rack-mini-profiler'
-
-Rack::MiniProfilerRails.initialize!(Rails.application)
-
-Rack::MiniProfiler.config.position = 'left'
-Rack::MiniProfiler.config.start_hidden = false
+# require 'rack-mini-profiler'
+#
+# Rack::MiniProfilerRails.initialize!(Rails.application)
+#
+# Rack::MiniProfiler.config.position = 'left'
+# Rack::MiniProfiler.config.start_hidden = false
