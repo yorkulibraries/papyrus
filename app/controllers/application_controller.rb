5ca452aaf62ed7cca7a538df364b76513866a979
Taras Danylak <tdanylak@yorku.ca>
2017-07-21 14:17:16 -0400

    Removed profiler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_action :miniprofiler
+  #before_action :miniprofiler
 
   private
 
-  def miniprofiler
-    if PapyrusSettings.profiler_enable == PapyrusSettings::TRUE && (current_user && current_user.role != User::STUDENT_USER)
-      Rack::MiniProfiler.authorize_request
-    else
-      Rack::MiniProfiler.deauthorize_request
-    end
-  end
+  # def miniprofiler
+  #   if PapyrusSettings.profiler_enable == PapyrusSettings::TRUE && (current_user && current_user.role != User::STUDENT_USER)
+  #     Rack::MiniProfiler.authorize_request
+  #   else
+  #     Rack::MiniProfiler.deauthorize_request
+  #   end
+  # end
 
 end
