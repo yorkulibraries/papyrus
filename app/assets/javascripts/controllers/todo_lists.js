b5e629e07b1d091eae9c93968ed3dc2b3c7dbb13
Taras Danylak <tdanylak@yorku.ca>
2020-09-04 09:31:27 -0400

    days of messing about, half way done

--- app/assets/javascripts/controllers/todo_lists.js
+++ app/assets/javascripts/controllers/todo_lists.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
-  $(".todo_list_status, .todo_item_status").click(function() {    
+  $(".todo_list_status, .todo_item_status").click(function() {
+    console.log("SUBMITTING")
     $(this).parents('form:first').submit();
+
   });
 
 
