9682db4ca70a46da220181e5c4f0f97385ec94db
Tuan Nguyen <amtuannguyen@gmail.com>
2022-05-31 19:40:51 -0400

    remove secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Papyrus::Application.config.secret_key_base = '43ff3721d6c38ed41f122c228d5f9aacadb38fdaeb12ac9d2256941aa6ab7eee01947c952dc73820dd1ec7b85ae676aeaa322913f7f6aaff524969d59a08180a'
