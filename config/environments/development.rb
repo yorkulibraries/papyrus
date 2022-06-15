3f23f4be4cab12f5707f7bd4d39f227b41d03536
Tuan Nguyen <amtuannguyen@gmail.com>
2022-06-15 13:07:02 -0400

    use mailinator for development emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,9 +23,11 @@ Rails.application.configure do
 
 
   # set delivery method to :smtp, :sendmail or :test
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'papyrus@mailinator.com'}
+  config.action_mailer.default_url_options = { host: "papyrus.me.ca" }
 
   # Expands the lines which load the assets
   config.assets.debug = true
