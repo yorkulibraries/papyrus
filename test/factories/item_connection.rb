b33f03c46f48ced3ade70ce366161a9d8b42aa7f
Luis Zambrano <lzambrano@yaraguasoluciones.com>
2022-06-15 15:02:18 -0400

    Fixing files fixtures creation.

--- test/factories/item_connection.rb
+++ test/factories/item_connection.rb
@@ -1,9 +1,7 @@
 FactoryGirl.define do
-
   factory :item_connection do
-    expires_on  1.year.from_now.to_s(:db)
+    expires_on 1.year.from_now.to_fs(:db)
     association :item, factory: :item
     association :student, factory: :student
   end
-  
 end
