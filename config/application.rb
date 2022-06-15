67bf45e73954ee7410c187428fcf6146f42c52f7
Luis Zambrano <lzambrano@yaraguasoluciones.com>
2022-06-15 13:52:04 -0400

    Updating Gemfile, rails version, and fixing helper call.

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Papyrus
 
     # Initialize configuration defaults for originally generated Rails version.
     config.action_mailer.delivery_job = "ActionMailer::MailDeliveryJob" 
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     config.filter_parameters += [:password]
 
