7c4e884833b75dd1a2b00a2969c850e7832f0647
Luis Zambrano <lzambra2@yorku.ca>
2022-06-30 15:41:11 -0400

    user authentication wip.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -40,7 +40,7 @@ class LoginController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    if user
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: 'Logged in!'
     else
