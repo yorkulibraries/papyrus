c91f33c51a9cb6c15497ff43bb729bde75493a17
Luis Zambrano <lzambra2@yorku.ca>
2022-06-30 10:20:03 -0400

    logout saml and clearing session.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,7 +3,7 @@ class LoginController < ApplicationController
     username = request.headers[PapyrusSettings.auth_cas_header]
     alt_username = request.headers[PapyrusSettings.auth_cas_header_alt]
 
-    users = User.unblocked.where("username = ? OR username = ?", username, alt_username)
+    users = User.unblocked.where('username = ? OR username = ?', username, alt_username)
 
     if users.size == 1
       user = users.first
@@ -11,30 +11,38 @@ class LoginController < ApplicationController
       session[:user_id] = user.id
       session[:username] = user.name
 
-      # if user.username != alt_username
-      #   user.update_attribute(:username, alt_username)
-      # end
-
       user.active_now!(User::ACTIVITY_LOGIN)
 
       if user.role == User::STUDENT_USER
         redirect_to my_student_portal_path
       else
-        redirect_to root_url, notice: "Logged in!"
+        redirect_to root_url, notice: 'Logged in!'
       end
     else
-      flash.now.alert = "Invalid email or password"
+      flash.now.alert = 'Invalid email or password'
 
-      render layout: "simple"
+      render layout: 'simple'
     end
   end
 
   def destroy
-    session[:user_id] = nil
-    session[:terms_accepted] = nil
-    cookies.delete("mayaauth", domain: PapyrusSettings.auth_cookies_domain)
-    cookies.delete("pybpp", domain: PapyrusSettings.auth_cookies_domain)
+    saml_sign_out = authenticated_with_saml?
+    cookies.delete('mayaauth', domain: PapyrusSettings.auth_cookies_domain)
+    cookies.delete('pybpp', domain: PapyrusSettings.auth_cookies_domain)
+    reset_session
+    if saml_sign_out
+      redirect_to destroy_saml_user_session_path
+    else
+      # redirect_to root_path
+      redirect_to PapyrusSettings.auth_cas_logout_redirect, allow_other_host: true
+    end
+  end
 
-    redirect_to PapyrusSettings.auth_cas_logout_redirect, allow_other_host: true
+  private
+
+  def authenticated_with_saml?
+    warden.session(:user)[:strategy] == :saml_authenticatable
+  rescue Warden::NotAuthenticated
+    false
   end
 end
