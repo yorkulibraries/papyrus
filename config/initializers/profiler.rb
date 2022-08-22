ff89fc945bd8ea68a7eb95ac047cd958a3256b1b
Luis Zambrano <lzambra2@yorku.ca>
2022-08-22 13:43:20 -0400

    fasting and updating the Gemfile.

--- config/initializers/profiler.rb
+++
@@ -1,6 +0,0 @@
-# require 'rack-mini-profiler'
-#
-# Rack::MiniProfilerRails.initialize!(Rails.application)
-#
-# Rack::MiniProfiler.config.position = 'left'
-# Rack::MiniProfiler.config.start_hidden = false
