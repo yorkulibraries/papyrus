3e962a84eec69a4856fafb7caf8b8b2402d18a22
Tuan Nguyen <amtuannguyen@gmail.com>
2023-04-04 16:33:24 -0400

    fix undefined method find_asset in acquisitions_mailer/send_acquisition_request.html.erb by setting config.assets.compile = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Papyrus::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
