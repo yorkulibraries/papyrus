6eacc722b90bc68aedfacabd088606d2c462cd6e
Luis Zambrano <lzambra2@yorku.ca>
2023-12-01 13:19:30 -0500

    Bypassing authentication. Initial test for #34

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: {
+  driven_by :custom_headers_driver, using: :chrome, screen_size: [1400, 1400], options: {
     browser: :remote,
     url: "http://#{ENV.fetch("SELENIUM_SERVER")}:4444"
   }
+  def setup
+    @user = create(:user, username: "TEST")
+  end
 end
