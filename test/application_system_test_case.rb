4c6294b6738043aaea9d0436e3e332be718458ce
Luis Zambrano <lzambra2@yorku.ca>
2023-12-01 10:50:21 -0500

    Adding setting to run system test. #34 #35

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: {
+    browser: :remote,
+    url: "http://#{ENV.fetch("SELENIUM_SERVER")}:4444"
+  }
 end
