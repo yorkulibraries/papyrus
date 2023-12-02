b30bc10ad411cc6dddd1b0f5a06d785eb78aaed0
Luis Zambrano <lzambra2@yorku.ca>
2023-12-02 14:45:10 -0500

    Excluding selenium for absence of this.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :custom_headers_driver, using: :chrome, screen_size: [1400, 1400], options: {
     browser: :remote,
-    url: "http://#{ENV.fetch("SELENIUM_SERVER")}:4444"
+    url: "http://#{ENV.fetch('SELENIUM_SERVER')}:4444"
   }
   def setup
-    @user = create(:user, username: "TEST")
+    @user = create(:user, username: 'TEST')
   end
 end
