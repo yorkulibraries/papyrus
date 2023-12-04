cbd4f8f7435e9c051a862bd04c924183356d73da
Luis Zambrano <lzambra2@yorku.ca>
2023-12-04 16:19:02 -0500

    Formatting authentication form for devise. #41

--- test/system/home_test.rb
+++ test/system/home_test.rb
@@ -1,9 +1,17 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class HomeTest < ApplicationSystemTestCase
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit root_url
-    assert_selector "body", text: "Register Student"
+    assert_selector 'body', text: 'Register Student'
+    click_link('Register Student')
+    find(:xpath, "//i[@class='fa-search fas']").click
+    find_field('Example').value
+    # page.save_screenshot('/usr/src/app/screenshot2.png')
+
+    # within(:xpath, "//input[@id='omni_search_field']") do
+    #   fill_in 'Example', with: "Search Items or Students"
+    # end
   end
 end
