a5e9adc405720eecf0011ecc13888dee2ba3352a
Luis Zambrano <lzambra2@yorku.ca>
2023-12-05 19:01:19 -0500

    Added test for item creation + modal search #35 + navbar menu #34

--- test/system/home_test.rb
+++ test/system/home_test.rb
@@ -1,7 +1,31 @@
 require 'application_system_test_case'
 
 class HomeTest < ApplicationSystemTestCase
-  test 'visiting the index' do
+  test 'Display menu, adding an Item, search item through modal' do
+    ITEM_NAME = 'York manual'
     assert_selector 'body', text: 'Register Student'
+
+    # Dropdown menu
+    click_link('New Item')
+    click_link('Blank Item')
+
+    # Adding item
+    fill_in('Full Title', with: ITEM_NAME)
+    select('Book', from: 'Item Type')
+    click_button('Create Item')
+
+    # Search in modal
+    assert_no_selector '#omni_search_modal.visible'
+    find('#global_search').click
+
+    # Wait for the modal to be visible
+    assert_selector '#omni_search_modal'
+
+    fill_in 'Search Items or Students', with: ITEM_NAME
+    find('#omni_search_button').click
+    click_link(ITEM_NAME)
+
+    # find('button.btn-secondary[data-bs-dismiss="modal"]').click
+    # page.save_screenshot('/usr/src/app/example.png')
   end
 end
