b5b6a05efc843f05ad9efe4fdb00877e1b6bfc5c
Tuan Nguyen <amtuannguyen@gmail.com>
2025-01-11 11:54:57 -0500

    update import script

--- bin/import_db.sh
+++ bin/import_db.sh
@@ -4,7 +4,7 @@ dump=$1
 echo 'SET foreign_key_checks = 0;' > drop.sql 
 echo 'show tables;' | rails db -p | grep -v "Tables_in_" | xargs -i -n 1 echo "DROP TABLE IF EXISTS \`{}\`;" >> drop.sql 
 
-[ -f "$dump" ] &&  zcat -f "$dump" | sed '/^DROP DATABASE \|^CREATE DATABASE \|^USE `/d'  | sed 's/ datetime / datetime(6) /'  > import.sql 
+[ -f "$dump" ] &&  zcat -f "$dump" | sed '/^DROP DATABASE \|^CREATE DATABASE /d'  > import.sql 
 
 cat drop.sql | rails db -p
 cat import.sql | rails db -p
